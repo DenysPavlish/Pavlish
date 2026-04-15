d36f3281f3df4f939ad313b7b36b3569ad0ff3ee
DenysPavlish <denyspavlish23@lfkhpp.ukr.education>
2026-04-15 13:44:24 +0300

    Add files via upload

---
+++ laba 13/13.1.1.py
@@ -0,0 +1,6 @@
+import search_utils
+
+lst = [10, 20, 30, 40, 50]
+
+print(search_utils.find_index(lst, 30))  
+print(search_utils.find_index(lst, 99))  
---
+++ laba 13/search_utils.py
@@ -0,0 +1,5 @@
+def find_index(lst, element):
+    for i in range(len(lst)):
+        if lst[i] == element:
+            return i
+    return -1
